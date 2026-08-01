8d8765ece9234efa3890ae5a8a9d226d9a3bf5ae
tommyvercettiseh <tommyvercettiseh@gmail.com>
2026-08-01 06:38:36 +0200

    Enable fullscreen benchmark patch

--- run_v06.py
+++ run_v06.py
@@ -15,6 +15,7 @@ from ai_mouse_lab.v071 import apply_patch as apply_v071_patch
 from ai_mouse_lab.v072 import apply_patch as apply_v072_patch
 from ai_mouse_lab.v074 import apply_patch as apply_v074_patch
 from ai_mouse_lab.v075 import apply_patch as apply_v075_patch
+from ai_mouse_lab.v076 import apply_patch as apply_v076_patch
 
 
 def main() -> None:
@@ -29,6 +30,7 @@ def main() -> None:
     apply_v072_patch(original_app)
     apply_v074_patch(original_app)
     apply_v075_patch(original_app)
+    apply_v076_patch(original_app)
     ctk.set_appearance_mode("dark")
     original_app.App().mainloop()
 
